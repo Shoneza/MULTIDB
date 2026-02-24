22c533aab8822316f0e26145090406bdb26ee760
UyuyuyItsTooBig <108665496+UyuyuyItsTooBig@users.noreply.github.com>
2026-02-24 13:30:30 +0700

    Merge1

--- asean-paragames/app/prototype/layout.tsx
+++ asean-paragames/app/prototype/layout.tsx
@@ -8,33 +8,36 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const pathname = usePathname();
+  const showSidebar = pathname !== '/prototype/tourinfo';
 
   return (
     <html lang="en">
       <body className={''}>
-        <div className="flex w-full h-screen bg-gray-900 text-white">
+        <div className="flex w-full h-full bg-gray-900 text-white">
           {/* Sidebar */}
-          <aside className="w-48 bg-black p-6 border-r border-gray-700">
-            <div className="mb-8">
-              <h2 className="text-cyan-400 font-bold text-lg">ASEAN</h2>
-              <h2 className="text-cyan-400 font-bold text-lg">PARAGAMES 2025</h2>
-            </div>
-            <nav className="space-y-4">
-              <ul className="space-y-2">
-                <li className={`${pathname === '/admin/requests' ? 'bg-cyan-400 font-bold text-black' : ''} py-2 px-3 text-white font-semibold rounded`}>
-                  <Link href="/admin/requests">
-                    Requests
-                  </Link>
-                </li>
-                <li className={`${pathname === '/admin/tournament-list' ? 'bg-cyan-400 font-bold text-black' : ''} py-2 px-3 text-white font-semibold rounded`}>
-                  <Link href="/admin/tournament-list">
-                    Tournament List
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </aside>
-          <main style={{ flex: 1, padding: "2rem" }}>{children}</main>
+          {showSidebar && (
+            <aside className="w-48 bg-black p-6 border-r border-gray-700">
+              <div className="mb-8">
+                <h2 className="text-cyan-400 font-bold text-lg">ASEAN</h2>
+                <h2 className="text-cyan-400 font-bold text-lg">PARAGAMES 2025</h2>
+              </div>
+              <nav className="space-y-4">
+                <ul className="space-y-2">
+                  <li className={`${pathname === '/admin/requests' ? 'bg-cyan-400 font-bold text-black' : ''} py-2 px-3 text-white font-semibold rounded`}>
+                    <Link href="/admin/requests">
+                      Requests
+                    </Link>
+                  </li>
+                  <li className={`${pathname === '/admin/tournament-list' ? 'bg-cyan-400 font-bold text-black' : ''} py-2 px-3 text-white font-semibold rounded`}>
+                    <Link href="/admin/tournament-list">
+                      Tournament List
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </aside>
+          )}
+          <main className={showSidebar ? "flex-1" : "w-full"} style={{ padding: "2rem" }}>{children}</main>
         </div>
       </body>
     </html>
